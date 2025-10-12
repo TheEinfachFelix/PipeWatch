0511c598b5e038c1cd4c88bf01cca0fff199a3bb
TheEinfachFelix <The.Einfach.Felix@gmail.com>
2025-10-12 17:09:33 +0200

    working msu

--- Software/PipeObserver/src/msu.cpp
+++ Software/PipeObserver/src/msu.cpp
@@ -2,43 +2,39 @@
 
 
 /*
-// This example shows the 3 axis acceleration.
+#include <Arduino.h>
 #include "LIS3DHTR.h"
-#include <Wire.h>
-LIS3DHTR<TwoWire> LIS; //IIC
-#define WIRE Wire
+#include "SPI.h"
+#include "conf.hpp"  // <-- config einbinden
+
+// LIS über SPI
+LIS3DHTR<SPIClass> LIS;
 
 void setup() {
-    Serial.begin(115200);
-    while (!Serial) {};
-    LIS.begin(WIRE); //IIC init dafault :0x18
-    //LIS.begin(WIRE, 0x19); //IIC init
-    delay(100);
-    //  LIS.setFullScaleRange(LIS3DHTR_RANGE_2G);
-    //  LIS.setFullScaleRange(LIS3DHTR_RANGE_4G);
-    //  LIS.setFullScaleRange(LIS3DHTR_RANGE_8G);
-    //  LIS.setFullScaleRange(LIS3DHTR_RANGE_16G);
-    //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_1HZ);
-    //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_10HZ);
-    //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_25HZ);
-    LIS.setOutputDataRate(LIS3DHTR_DATARATE_50HZ);
-    //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_100HZ);
-    //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_200HZ);
-    //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_1_6KHZ);
-    //  LIS.setOutputDataRate(LIS3DHTR_DATARATE_5KHZ);
-    LIS.setHighSolution(true); //High solution enable
+  Serial.begin(115200);
+  while (!Serial);
+
+  // SPI starten mit MSU Pins aus config.hpp
+  SPI.begin(MSU_SPC_PIN, MSU_SAO_PIN, MSU_SDI_PIN); // CLK, MISO, MOSI
+  LIS.begin(SPI, MSU_CS_PIN);                        // CS Pin aus config.hpp
+
+  LIS.openTemp();         // ADC3 Temp Enable/Disable
+  delay(100);
+
+  LIS.setOutputDataRate(LIS3DHTR_DATARATE_50HZ);
 }
+
 void loop() {
     if (!LIS) {
         Serial.println("LIS3DHTR didn't connect.");
         while (1);
         return;
     }
-    //3 axis
-    Serial.print("x:"); Serial.print(LIS.getAccelerationX()); Serial.print("  ");
-    Serial.print("y:"); Serial.print(LIS.getAccelerationY()); Serial.print("  ");
-    Serial.print("z:"); Serial.println(LIS.getAccelerationZ());
 
+    // Temperatur auslesen
+    Serial.print("temp:"); 
+    Serial.println(LIS.getTemperature());
     delay(500);
 }
+
 */
